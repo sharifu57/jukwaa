2abf271b5141dd44e5f1ab2fcb3ed275c263ba45
sharifu majid <sharifumajid3@gmail.com>
2024-04-03 14:33:00 +0300

    push production4

--- jukwaa/settings.py
+++ jukwaa/settings.py
@@ -82,6 +82,12 @@ MIDDLEWARE = [
     "easyaudit.middleware.easyaudit.EasyAuditMiddleware"
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "*",
+]
+
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
