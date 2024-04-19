47f34cd8580685a8857c76493a84d2c37e23fcac
sharifu majid <sharifumajid3@gmail.com>
2024-04-19 15:09:39 +0300

    path

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Featured(BaseModel):
-    title = models.CharField(null=True, blank=True, max_length=300)
+    title = models.CharField(null=True, blank=True, max_length=200)
     description = models.TextField(null=True, blank=True)
-    picture = models.ImageField(upload_to="/featured", null=True, blank=True)
+    picture = models.ImageField(upload_to="featured", null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
@@ -20,7 +20,7 @@ class Featured(BaseModel):
 class Story(BaseModel):
     title = models.CharField(max_length=300, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to="/images", null=True, blank=True)
+    image = models.ImageField(upload_to="images", null=True, blank=True)
     crated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
