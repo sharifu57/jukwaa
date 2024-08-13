a0bcf4ebe91705c08a5c9307d04c4a62dd55d577
sharifu majid <sharifumajid3@gmail.com>
2024-08-13 21:35:36 +0300

    jwt token

--- jukwaa/urls.py
+++ jukwaa/urls.py
@@ -21,6 +21,10 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -47,6 +51,8 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # end of the lines url
 ]
 from django.conf.urls.static import static
